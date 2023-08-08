1f1e2909c4efe5bfc4fbf593488861c585c62ce1
IvanMuzik <ivanmuz86@gmail.com>
2023-08-08 09:40:45 +0300

    Initial Commit

---
+++ gitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitApp
+//
+//  Created by Ivan Muzalevski on 8.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
